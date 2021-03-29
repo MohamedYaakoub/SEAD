e4c0f3e7709aab0bda922ac8483e53cb85dc5893
Mohamed Yaakoub <mohamedelsayedabdelalimmohamedyaakoub@student.tudelft.nl>
2021-03-30 01:26:54 +0200

    it plots

--- Scissor plot.py
+++ Scissor plot.py
@@ -2,20 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Global variables
-A = 8.89  # aspect ration Main wing
+A = 8.89 * 1.15  # aspect ration Main wing
 A_h = 7.88
 taper_mw = 0.356
 taper_h = 0.410
-c = 3.17  # I used MAC #Ø: should be 2.934 I think.
+c = 3  # I used MAC #Ø: should be 2.934 I think.
 lh = 28.55 - 0.08861 * 28.55 - 12.49 + 1.038 - 0.25 * c  # length of aircraft - approx 3/4 tail - AX0 + AX0 to lemac - quarter chord
 lf = 26.5
-lf_n = lh  ###### CHANGEEE###########
 
 b = 26.21  # wing span probably!!
 bf = 3.56  # fuselage diameter - I put width #Assumed circular
 bn = 1.4
 ln = 2.6    #I put length of the engine
 hf = bf
+
+lf_n = 28.55 - lh - ln
 
 VhV2 = 1  # T-tail
 lambda_quarter_chord_mw = np.radians(15)  # main wing
@@ -35,14 +36,21 @@ def lambda_n(n, m, phi_m, Aspect_ratio, taper):
 lambda_half_chord_mw = lambda_n(50, 25, lambda_quarter_chord_mw, A, taper_mw)
 lambda_half_chord_h = lambda_n(50, 25, lambda_quarter_chord_h, A_h, taper_h)
 lambda_LE_mw = lambda_n(0, 25, lambda_quarter_chord_mw, A, taper_mw)
+lambda_LE_h = lambda_n(0, 25, lambda_quarter_chord_h, A_h, taper_h)
+
+# print(np.degrees(lambda_half_chord_mw))
+# print(np.degrees(lambda_half_chord_h))
+# print(np.degrees(lambda_LE_mw))
+# print(np.degrees(lambda_LE_h))
+
 eta_mw = 0.95
 eta_h = 0.95
-mach_mw = 0.730  # at cruise alt
-mach_h = mach_mw
+mach_cruise = 0.730  # at cruise alt
+mach_landing = 0.189
 
 CL = 3.43  # wing lift coefficient at landing (all flaps deployed) I used at L/D
 CL_h = - 0.35 * A_h ** (1 / 3)
-CL0 = 1  # CL0 is the lift coefficient of the flapped wing at zero angle of attack ##CHANGE!!!
+CL0 = 0.15  # CL0 is the lift coefficient of the flapped wing at zero angle of attack ##CHANGE!!!
 
 
 def fun_CL_a(mach, wing_area, eta, lambda_half_chord):  # will be used for both the main wing and the horizontal tail
@@ -52,27 +60,24 @@ def fun_CL_a(mach, wing_area, eta, lambda_half_chord):  # will be used for both 
     return result
 
 
-# CL_a_mw = fun_CL_a(mach_mw, S, eta_mw, lambda_half_chord_mw)
-CL_a_h = fun_CL_a(mach_h, S_h, eta_h, lambda_half_chord_h)
-
 
 def fun_CL_aAh(mach, wing_area, eta, lambda_half_chord):
     CL_aW = fun_CL_a(mach, wing_area, eta, lambda_half_chord)
+    print(CL_aW, mach)
     result = CL_aW * (1 + 2.15 * bf / b) * S_net / S + (np.pi / 2) * (bf ** 2 / S)
+    # print(CL_aW * (1 + 2.15 * bf / b) * S_net / S, (np.pi / 2) * (bf ** 2 / S), mach)
     return result
 
 
-CL_aAh = fun_CL_aAh(mach_mw, S, eta_mw, lambda_half_chord_mw)
-
-
 def wing_downwash():
-    weird_distance = lh / 3  # check slide 42  ##CHANGE!!!
+    CL_aAh = fun_CL_aAh(mach_cruise, S, eta_mw, lambda_half_chord_mw)
+    weird_distance = 4.2  # check slide 42  ##CHANGE!!!
 
     r = 2 * lh / b
     mtv = 2 * weird_distance / b
     k_e = (0.1124 + 0.1265 * lambda_quarter_chord_mw + 0.1766 * lambda_quarter_chord_mw ** 2) / r ** 2 + 0.1024 / r + 2
     k_e0 = 0.1124 / r ** 2 + 0.1024 / r + 2
-    # CL_aW = fun_CL_a(mach_mw, 0, 0, 0)
+    # CL_aW = fun_CL_a(mach_cruise, 0, 0, 0)
     result = k_e / k_e0 * (r / (r ** 2 + mtv ** 2) * 0.4876 / np.sqrt(r ** 2 + 0.6319 + mtv ** 2) +
                            (1 + (r ** 2 / (r ** 2 + 0.7915 + 5.0734 * mtv ** 2)) ** 0.3113) * (
                                    1 - np.sqrt(mtv ** 2 / (1 + mtv ** 2)))) * CL_aAh / (np.pi * A)
@@ -80,52 +85,65 @@ def wing_downwash():
 
 
 def fun_Cm_ac():
-    Cm_0 = 1 ##CHANGE!!!
+    CL_aAh = fun_CL_aAh(mach_landing, S, eta_mw, lambda_half_chord_mw)
+    Cm_0 = -0.06
     Cm_ac_w = Cm_0 * (A * np.cos(lambda_LE_mw) ** 2 / (A + 2 * np.cos(lambda_LE_mw)))
     Cm_ac_fus = -1.8 * (1 - 2.5 * bf / lf) * (np.pi * bf * hf * lf / (4 * S * c)) * (
-            CL0 / CL_aAh)  # compute CL_aAH for low speed  ##CHANGE!!!
+            CL0 / CL_aAh)  # compute CL_aAH for low speed
 
     def fun_Cm_ac_flaps():
-        u1 = 0.2  # check slide 18 ##CHANGE!!!
+        u1 = 0.20125  # check slide 18
         u2 = 0.8  # recheck slide 19
         u3 = 0.04  # recheck slide 19
-        cprime_c = 1.2    ##CHANGE!!!
-        delta_Cl_max = 0.2  # the airfoil lift coefficient increase due to flap extension at landing condition (estimated in the wing design module)  ##CHANGE!!!
+        cprime_c = 1.233    ##CHANGE!!!
+        delta_Cl_max = 1.6  # the airfoil lift coefficient increase due to flap extension at landing condition (estimated in the wing design module)  ##CHANGE!!!
 
         Cm_025 = u2 * (- u1 * delta_Cl_max * cprime_c - (CL + delta_Cl_max * (1 - Swf_S)) * 1 / 8 * cprime_c * (
                 cprime_c - 1)) + \
                  0.7 * A / (1 + 2 / A) * u3 * delta_Cl_max * np.tan(lambda_quarter_chord_mw)
-        return Cm_025 - CL * (0.25 - fun_x_ac() / c)
+        return Cm_025 - CL * (0.25 - fun_x_ac(mach_landing) / c)
 
     Cm_ac_flaps = fun_Cm_ac_flaps()
-
     return Cm_ac_w + Cm_ac_fus + Cm_ac_flaps  # nacelle contribution is still remaining
 
 
-def fun_x_ac():
-    wing = 0.28  # using lambda = 17.8 and A = 8.89
+def fun_x_ac(mach):
+    CL_aAh = fun_CL_aAh(mach, S, eta_mw, lambda_half_chord_mw)
+    if mach == 0.73:
+        wing = 0.28  # using lambda = 17.8 and A = 8.89
+    else:
+        wing = 0.27
     fus_1 = -1.8 / CL_aAh * (bf * hf * lf_n) / (S * c)
-    fus_2 = 0.273 / (1 + taper_mw) * (bf * S / b * (b - bf)) / (c ** 2 * (b + 2.15 * bf))
+    fus_2 = 0.273 / (1 + taper_mw) * (bf * S / b * (b - bf)) / (c ** 2 * (b + 2.15 * bf)) * np.tan(lambda_quarter_chord_mw)
     nacelles = -4 * (bn ** 2 * ln) / (S * c * CL_aAh) * 4
     return wing + fus_1 + fus_2 + nacelles
 
 
-x_ac = fun_x_ac()
 
 
 def plot_stability():
+    CL_a_h = fun_CL_a(mach_cruise, S_h, eta_h, lambda_half_chord_h)
+    x_ac = fun_x_ac(mach_cruise)
+    CL_aAh = fun_CL_aAh(mach_cruise, S, eta_mw, lambda_half_chord_mw)
+
     de_da = wing_downwash()
+    print(de_da)
     # CL_ah = fun_CL_a(0, 0, 0, 0)
 
     m = 1 / (CL_a_h / CL_aAh * (1 - de_da) * lh / c * VhV2)  # slope
     q = -(x_ac - 0.05) * m
+    q_2 = -x_ac * m
     x_plot = np.linspace(0, 1, 100)
     y_plot = m * x_plot + q
+    y_plot_2 = m * x_plot + q_2
     plt.plot(x_plot, y_plot, label='Stability')
+    plt.plot(x_plot, y_plot_2, label='Neutral Stability', linestyle= "dashed")
 
 
 def plot_controllability():
+    x_ac = fun_x_ac(mach_landing)
     Cm_ac = fun_Cm_ac()
+    print(Cm_ac, "asdasd")
     CL_AH = CL  # CLA-h can be approximated with the wing lift coefficient (in this case at landing condition), ignoring the minor contribution of the fuselage.
     m = 1 / (CL_h / CL_AH * lh / c * VhV2)
     q = (Cm_ac / CL_AH - x_ac) / (CL_h / CL_AH * lh / c * VhV2)
@@ -142,8 +160,10 @@ if __name__ == '__main__':
     plt.xlabel(r'$X_{cg}$ / MAC')
     plt.axhline(y = 0, color='black')
     plt.axvline(x= 0, color='black')
+    plt.axhline(y=S_h/S,label='Current horizontal wing size ratio', color = 'black', linestyle = 'dotted')
     plt.legend()
     plt.ylim(0)
-    plt.xlim(0)
+    plt.xlim(0,1)
+    plt.grid()
     plt.show()
 
